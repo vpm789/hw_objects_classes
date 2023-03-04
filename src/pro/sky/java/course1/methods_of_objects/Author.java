492c21a61a36484492ed9586642e3d369c019a01
vpm789 <vpm789@gmail.com>
2023-03-04 07:52:53 +0300

    methods_of_objects

--- src/pro/sky/java/course1/methods_of_objects/Author.java
+++ src/pro/sky/java/course1/methods_of_objects/Author.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.methods_of_objects;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String lastName;
@@ -21,5 +23,18 @@ public class Author {
     public String toString() {
         return name + " " +
                 lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
     }
 }
