c74658124cb7a1bbd432d3b0f05722dd42202923
vpm789 <vpm789@gmail.com>
2023-03-04 17:26:00 +0300

    methods_of_objects_difficult_tasks

--- src/pro/sky/java/course1/classes_objects/Author.java
+++ src/pro/sky/java/course1/classes_objects/Author.java
@@ -16,4 +16,6 @@ public class Author {
         return this.lastName;
     }
 
+
+
 }
---
+++ src/pro/sky/java/course1/methods_of_objects/Library.java
@@ -0,0 +1,55 @@
+package pro.sky.java.course1.methods_of_objects;
+
+import java.util.Arrays;
+
+public class Library {
+    private Book[] books;
+    private int size;
+
+    public Library(int size) {
+        this.books = new Book[size];
+    }
+
+    public void addBook(Book book) {
+        if (size >= books.length) {
+            System.out.println("Нельзя добавить книгу, закончилось место");
+        }
+        Book newBook = book;
+        books[size++] = newBook;
+    }
+
+    public void printAllBooks() {
+        for (int i = 0; i < size; i++) {
+            Book book = books[i];
+            System.out.println(book.getAuthor().getName() + " " + book.getAuthor().getLastName() + ": " + book.getTitle() + ": " + book.getYear());
+        }
+    }
+
+    public void printBooks(String title) {
+        for (int i = 0; i < size; i++) {
+            Book book = books[i];
+            boolean same = book.getTitle().equals(title);
+            if (same == true) {
+                System.out.println(book.getAuthor().getName() + " " + book.getAuthor().getLastName() + ": " + book.getTitle() + ": " + book.getYear());
+            } else if (i == size - 1 && same == false) {
+                System.out.println("Книги с таким названием нет");
+            }
+        }
+    }
+
+    public void changeYear(String title, int year) {
+        for (int i = 0; i < size; i++) {
+            Book book = books[i];
+            if (book.getTitle().equals(title)) {
+                book.setYear(year);
+                System.out.println(book.getAuthor().getName() + " " + book.getAuthor().getLastName() + ": " + book.getTitle() + ": " + book.getYear());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Library: " + Arrays.toString(books).replaceAll("(^\\[)|(\\, )|(\\])", "\n");
+    }
+
+}
